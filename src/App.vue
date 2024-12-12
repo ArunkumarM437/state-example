aad53ed3b57c6fb3c02de1f7e9a2364edf0f090b
Arunkumar-M174 <arunprince580@gmail.com>
2024-12-12 22:52:51 +0530

    progress VueX basics of 12/12/2024.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
+  <link href="https://fonts.googleapis.com/css2?family=Anybody:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
   <img alt="Vue logo" src="./assets/Daco_4176864.png">
-  <HelloWorld msg="Welcome to State-Management Vue.js App"/>
+  <usersData/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import usersData from './components/usersData.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    usersData
   }
 }
 // TO-DO
@@ -18,7 +19,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Anybody", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/usersData.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>Hello All Welcome Again</h3>
+    <div class="users">
+      <div v-for="user in allUsers" :key="user.id" class="eachuser">{{ user.name }}</div>
+    </div>
+  </div>
+</template>
+
+<script> 
+
+import { mapGetters } from 'vuex';
+
+export default {
+  name:"usersData",
+  computed : mapGetters(...['allUsers '])
+}
+</script>
+ 
+<style>
+
+</style>
