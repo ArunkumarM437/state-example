9acadde0ae3e0e412e3e3adfecbcf77c369ce79d
Arunkumar-M174 <arunprince580@gmail.com>
2025-01-27 23:48:22 +0530

    changes for images.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <link href="https://fonts.googleapis.com/css2?family=Anybody:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-  <img alt="Vue logo" src="./assets/Daco_4176864.png">
+  <img alt="Vue logo" src="./assets/racoon.png">
 
   <usersData/>
   <div class="wrapper">
